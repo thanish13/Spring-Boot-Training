39e8b0327c31230b03df661f7ac28e49b414501f
thanish13 <thanishmalai13@gmail.com>
2025-04-22 22:33:53 +0530

    Oauth2.0

--- 04-OAuth/src/main/java/org/example/config/SecurityConfig.java
+++ 04-OAuth/src/main/java/org/example/config/SecurityConfig.java
@@ -16,20 +16,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    @Order(1)
-    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(withDefaults()); // Enable OpenID Connect 1.0
-        return http.formLogin(withDefaults()).build();
-    }
-
-    @Bean
-    @Order(2)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                        .authenticated())
-                .formLogin(withDefaults());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
         return http.build();
     }
+
 }
