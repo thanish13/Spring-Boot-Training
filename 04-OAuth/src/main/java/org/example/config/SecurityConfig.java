18a50362c821b0c6532eca7624a3036d106c4d75
thanish13 <thanishmalai13@gmail.com>
2025-04-25 21:54:40 +0530

    Spring security filter

--- 04-OAuth/src/main/java/org/example/config/SecurityConfig.java
+++ 04-OAuth/src/main/java/org/example/config/SecurityConfig.java
@@ -1,31 +1,43 @@
 package org.example.config;
 
+import jakarta.servlet.Filter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.*;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> {
-            request.requestMatchers("/**/**/login").permitAll();
-            request.requestMatchers("/**/**/home").permitAll();
-            request.anyRequest().denyAll();
+            request.requestMatchers(antMatcher("/**/**/login")).permitAll();
         });
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        http.csrf((csrf) -> csrf
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .csrfTokenRequestHandler(requestHandler)
-        );
-        http.csrf(csrf -> {csrf.ignoringRequestMatchers("/**/**/home");
-            csrf.ignoringRequestMatchers("/**/**/home");
+//        http.csrf((csrf) -> csrf
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//        );
+        http.csrf(csrf -> {
+            csrf.ignoringRequestMatchers("/**/**/login");
+        });
+        Filter authenticationTokenFilter = new BasicAuthenticationFilter( x -> x);
+        http.addFilterBefore(authenticationTokenFilter, BasicAuthenticationFilter.class);
+        AuthenticationProvider tokenAuthenticationProvider = new DaoAuthenticationProvider();
+        http.authenticationProvider(tokenAuthenticationProvider);
+        http.authorizeHttpRequests(request -> {
+            request.anyRequest().authenticated();
         });
         return http.build();
     }
