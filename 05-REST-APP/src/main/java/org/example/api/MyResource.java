3f2c5b37965f12473051f3aecc9fd51b656f275e
thanish13 <thanishmalai13@gmail.com>
2025-05-18 22:53:45 +0530

    Swagger changes

--- 05-REST-APP/src/main/java/org/example/api/MyResource.java
+++ 05-REST-APP/src/main/java/org/example/api/MyResource.java
@@ -17,7 +17,7 @@ import org.example.model.Body;
     @Tag(name = "My Resource", description = "Operations related to My Resource")
     public class MyResource {
         @GET
-        @Produces(MediaType.APPLICATION_JSON)g
+        @Produces(MediaType.APPLICATION_JSON)
         @Operation(summary = "Get data", description = "Returns some data")
         @ApiResponses(value = {
                 @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Body.class))),
