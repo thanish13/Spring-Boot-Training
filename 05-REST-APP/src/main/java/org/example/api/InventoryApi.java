b1dbdf324e88dff4ddb40752a120c68354eea6a3
thanish13 <thanishmalai13@gmail.com>
2025-06-19 21:30:55 +0530

    Inventory Code

---
+++ 05-REST-APP/src/main/java/org/example/api/InventoryApi.java
@@ -0,0 +1,75 @@
+package org.example.api;
+
+import org.example.model.InventoryItem;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
+
+/**
+* Represents a collection of functions to interact with the API endpoints.
+*/
+@Path("/inventory")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2025-06-19T21:13:58.473124600+05:30[Asia/Calcutta]", comments = "Generator version: 7.12.0")
+public interface InventoryApi {
+
+    /**
+     * 
+     *
+     * @return List of inventory items
+     */
+    @GET
+    @Operation(summary = "Get all inventory items", description = "" )
+    Response inventoryGet();
+
+    /**
+     * 
+     *
+     * @param itemId 
+     * @return Item deleted
+     */
+    @DELETE
+    @Path("/{itemId}")
+    @Operation(summary = "Delete a specific inventory item", description = "" )
+    Response inventoryItemIdDelete(@PathParam("itemId") String itemId);
+
+    /**
+     * 
+     *
+     * @param itemId 
+     * @return Inventory item details
+     * @return Item not found
+     */
+    @GET
+    @Path("/{itemId}")
+    @Operation(summary = "Get a specific inventory item", description = "" )
+    Response inventoryItemIdGet(@PathParam("itemId") String itemId);
+
+    /**
+     * 
+     *
+     * @param itemId 
+     * @param inventoryItem 
+     * @return Item updated
+     */
+    @PUT
+    @Path("/{itemId}")
+    @Consumes({ "application/json" })
+    @Operation(summary = "Update a specific inventory item", description = "" )
+    Response inventoryItemIdPut(@PathParam("itemId") String itemId,@Valid @NotNull InventoryItem inventoryItem);
+
+    /**
+     * 
+     *
+     * @param inventoryItem 
+     * @return Item created
+     */
+    @POST
+    @Consumes({ "application/json" })
+    @Operation(summary = "Create a new inventory item", description = "" )
+    Response inventoryPost(@Valid @NotNull InventoryItem inventoryItem);
+}
---
+++ 05-REST-APP/src/main/java/org/example/model/InventoryItem.java
@@ -0,0 +1,144 @@
+package org.example.model;
+
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+
+
+@JsonTypeName("InventoryItem")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2025-06-19T21:13:58.473124600+05:30[Asia/Calcutta]", comments = "Generator version: 7.12.0")
+public class InventoryItem   {
+  private String id;
+  private String name;
+  private Integer quantity;
+  private Float price;
+
+  public InventoryItem() {
+  }
+
+  /**
+   **/
+  public InventoryItem id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  
+  @JsonProperty("id")
+  public String getId() {
+    return id;
+  }
+
+  @JsonProperty("id")
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  /**
+   **/
+  public InventoryItem name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  
+  @JsonProperty("name")
+  public String getName() {
+    return name;
+  }
+
+  @JsonProperty("name")
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   **/
+  public InventoryItem quantity(Integer quantity) {
+    this.quantity = quantity;
+    return this;
+  }
+
+  
+  @JsonProperty("quantity")
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  @JsonProperty("quantity")
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  /**
+   **/
+  public InventoryItem price(Float price) {
+    this.price = price;
+    return this;
+  }
+
+  
+  @JsonProperty("price")
+  public Float getPrice() {
+    return price;
+  }
+
+  @JsonProperty("price")
+  public void setPrice(Float price) {
+    this.price = price;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    InventoryItem inventoryItem = (InventoryItem) o;
+    return Objects.equals(this.id, inventoryItem.id) &&
+        Objects.equals(this.name, inventoryItem.name) &&
+        Objects.equals(this.quantity, inventoryItem.quantity) &&
+        Objects.equals(this.price, inventoryItem.price);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, quantity, price);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class InventoryItem {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    quantity: ").append(toIndentedString(quantity)).append("\n");
+    sb.append("    price: ").append(toIndentedString(price)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+
+}
+
