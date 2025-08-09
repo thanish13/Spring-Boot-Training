75b33ac8807279eea72febdf50fd3582ac9ac401
thanish13 <thanishmalai13@gmail.com>
2025-08-09 22:41:16 +0530

    Trying graphQL

--- 06-Spring-Web/src/main/java/com/example/demo/controller/PostController.java
+++ 06-Spring-Web/src/main/java/com/example/demo/controller/PostController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Post;
+import com.example.demo.model.Book;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -11,7 +12,17 @@ import java.util.List;
 public class PostController {
 
     @QueryMapping
-    public List<Post> recentPosts(@Argument int count, @Argument int offset) {
-        return null;
+    public List<Book> allBooks() {
+        return List.of(new Book("1","",""));
+    }
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return new Book("1","","");
+    }
+
+    @MutationMapping
+    public Book addBook(@Argument String title, @Argument String author) {
+        return new Book("1",title, author);
     }
 }
--- 06-Spring-Web/src/main/java/com/example/demo/model/Book.java
+++ 06-Spring-Web/src/main/java/com/example/demo/model/Book.java
@@ -1,8 +1,11 @@
 package com.example.demo.model;
 
-public class Post {
+public class Book {
     private String id;
     private String title;
     private String category;
     private String authorId;
+
+    public Book(String id, String title, String author) {
+    }
 }
