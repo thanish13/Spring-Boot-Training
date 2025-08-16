ea718ebe648437be3c50c3b5633019e907ee1c17
thanish13 <thanishmalai13@gmail.com>
2025-08-16 22:47:48 +0530

    BDD for GET and POST call

--- 06-Spring-Web/src/test/java/com/example/demo/bdd/SpringIntegrationTest.java
+++ 06-Spring-Web/src/test/java/com/example/demo/bdd/SpringIntegrationTest.java
@@ -1,50 +1,12 @@
 package com.example.demo.bdd;
 
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.ClassicHttpRequest;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-
-import java.io.IOException;
 
 @CucumberContextConfiguration
-@SpringBootTest(classes = TestConfig.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = TestConfig.class , webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SpringIntegrationTest {
 
-    @LocalServerPort
-    public int port;
 
-    public SpringIntegrationTest(){
-        System.out.println("Starting App in PORT : " + port);
-    }
 
-    public static CloseableHttpClient httpClient = HttpClients.createDefault();
-
-    // executeGet implementation
-    public HttpResponse executeGet(String url) throws IOException {
-
-        ClassicHttpRequest request = new HttpPost("http://localhost:" + port + url);
-        request.addHeader("content-type", "application/json");
-        HttpResponse response = httpClient.execute(request);
-        return response;
-    }
-
-    // executePost implementation
-    public HttpResponse executePost(String url, Object body) throws IOException {
-
-        ClassicHttpRequest request = new HttpPost("http://localhost:" + port + url);
-        request.addHeader("content-type", "application/json");
-        StringEntity entity = new StringEntity(body.toString());
-        request.setEntity(entity);
-        HttpResponse response = httpClient.execute(request);
-
-        return  response;
-
-    }
 }
--- 06-Spring-Web/src/test/java/com/example/demo/bdd/StepDefs.java
+++ 06-Spring-Web/src/test/java/com/example/demo/bdd/StepDefs.java
@@ -4,9 +4,17 @@ package com.example.demo.bdd;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.http.message.BasicHttpResponse;
 import org.junit.Assert;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
@@ -17,6 +25,34 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class StepDefs extends SpringIntegrationTest {
+
+    public int port = 8080;
+
+    public StepDefs(){
+        System.out.println("Starting StepDefs App in PORT : " + port);
+    }
+
+    public static CloseableHttpClient httpClient = HttpClients.createDefault();
+
+    // executeGet implementation
+    public HttpResponse executeGet(String url) throws IOException {
+
+        ClassicHttpRequest request = new HttpPost("http://localhost:" + port + "/api/books");
+        request.addHeader("content-type", "application/json");
+        return httpClient.execute(request);
+    }
+
+    // executePost implementation
+    public HttpResponse executePost(String url, Object body) throws IOException {
+
+        ClassicHttpRequest request = new HttpPost("http://localhost:" + port + url);
+        request.addHeader("content-type", "application/json");
+        StringEntity entity = new StringEntity(body.toString());
+        request.setEntity(entity);
+
+        return httpClient.execute(request);
+
+    }
 
     public HttpResponse response;
 
