3d2d69f7bb4ab9363d356f08d49038df5256aaaa
thanish13 <thanishmalai13@gmail.com>
2025-08-25 21:39:11 +0530

    Spring Oauth

--- 06-Spring-Web/src/main/java/com/example/demo/api/OauthApi.java
+++ 06-Spring-Web/src/main/java/com/example/demo/api/OauthApi.java
@@ -2,9 +2,11 @@ package com.example.demo.api;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public interface OauthApi {
 
     @GetMapping("/public")
--- 06-Spring-Web/src/main/java/com/example/demo/config/SecurityConfig.java
+++ 06-Spring-Web/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,25 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer:: jwt);
+                http
+                    .authorizeHttpRequests(auth -> auth.requestMatchers("http://localhost:8080/api/public").permitAll())
+                    .csrf(csrf -> csrf.ignoringRequestMatchers("http://localhost:8080/api/public").disable())
+                    .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
