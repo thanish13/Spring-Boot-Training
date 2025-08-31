304bbe8788e1c4fd223ccd5360b038f56d1893be
thanish13 <thanishmalai13@gmail.com>
2025-08-31 11:16:56 +0530

    Spring Oauth

---
+++ 04-OAuth/OAuth-Server/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication()
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(Main.class);
+    }
+}
