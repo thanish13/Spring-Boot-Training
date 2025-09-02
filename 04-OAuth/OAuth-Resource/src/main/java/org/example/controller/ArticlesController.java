68a31584ab0d9c7252b16b5ef1fe5a6610486740
thanish13 <thanishmalai13@gmail.com>
2025-09-02 06:30:38 +0530

    Spring Oauth - working

--- 04-OAuth/OAuth-Resource/src/main/java/org/example/controller/ArticlesController.java
+++ 04-OAuth/OAuth-Resource/src/main/java/org/example/controller/ArticlesController.java
@@ -1,12 +1,7 @@
 package org.example.controller;
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @RestController
 public class ArticlesController {
