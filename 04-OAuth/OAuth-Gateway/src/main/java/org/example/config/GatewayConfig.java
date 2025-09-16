8704e042179fc6adc82795a32da28a230becad53
thanish13 <thanishmalai13@gmail.com>
2025-09-16 21:43:05 +0530

    Spring Cloud

--- 04-OAuth/OAuth-Gateway/src/main/java/org/example/config/GatewayConfig.java
+++ 04-OAuth/OAuth-Gateway/src/main/java/org/example/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package org.example.config;
 
 
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -13,7 +14,6 @@ public class GatewayConfig {
         return routeLocatorBuilder.routes()
                 .route("Microservice1",r->r.path("/serviceA/**")
                         .uri("http://localhost:8081/"))
-                .route("Microservice2",r->r.path("/serviceB/**")
-                        .uri("http://localhost:8082/")).build();
+              .build();
     }
 }
