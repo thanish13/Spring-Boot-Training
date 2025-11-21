89260d1131f910a6bb4557b246d39a0b28e7159f
thanish13 <thanishmalai13@gmail.com>
2025-11-21 22:59:05 +0530

    Trying certs

--- 04-OAuth/OAuth-Server/src/main/java/org/example/ServerApp.java
+++ 04-OAuth/OAuth-Server/src/main/java/org/example/ServerApp.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication()
 public class ServerApp {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
         SpringApplication.run(ServerApp.class);
+
     }
 }
